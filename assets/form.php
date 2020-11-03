84d1ed0365fcc3d4151854b13c64ab0b5e6371e2
Zachary Ciatteo <zciatteo96@gmail.com>
2020-11-02 20:34:39 -0500

    add form functionalit

---
+++ assets/form.php
@@ -0,0 +1,7 @@
+<?php
+    
+    echo $_POST['name'];
+    echo $_POST['email'];
+    echo $_POST['phone'];
+    echo $_POST['reason'];
+?>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,71 @@
-"use strict";
 /*Remove no-js class*/
-var body = document.querySelector("body");
-body.classList.remove("no-js");
-
 var toggle = document.getElementById("nav-toggle"); //Navigation Toggle
 
-window.onresize = navToggle;
+var mainForm = document.getElementById("main-form");
+
+mainForm.addEventListener("submit", formSubmit);
 
 function navToggle() {
   if (window.innerWidth > 968) {
     toggle.checked = false;
   }
+}
+
+function formValidation() {
+  var pass = true;
+  var formElements = document.querySelectorAll(
+    "#main-form input, #main-form textarea"
+  );
+
+  formElements.forEach(function (item) {
+    if (item.value == "") {
+      item.closest(".form-input").classList.add("error");
+    } else {
+      item.closest(".form-input").classList.remove("error");
+    }
+  });
+
+  var formErrors = document.querySelectorAll("#main-form > .error");
+
+  if (formErrors.length != 0) {
+    pass = false;
+  }
+
+  return pass;
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  var formName = document.getElementById("name").value;
+  var formEmail = document.getElementById("email").value;
+  var formPhone = document.getElementById("phone").value;
+  var formReason = document.getElementById("reason").value;
+
+  if (!formValidation()) {
+    return false;
+  }
+
+  var params =
+    "name=" +
+    formName +
+    "&email=" +
+    formEmail +
+    "&phone=" +
+    formPhone +
+    "&reason=" +
+    formReason;
+
+  var ajax = new XMLHttpRequest();
+
+  ajax.open("POST", "assets/form.php", true);
+  ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  ajax.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert("yeah!");
+    }
+  };
+
+  ajax.send(params);
 }
